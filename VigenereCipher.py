0e9143ff8af9356463814faabab3ac4c439c1bff
Y4jn4s <167096479+Y4jn4s@users.noreply.github.com>
2025-02-17 08:35:52 +0530

    Code for Vigenere Cipher

---
+++ VigenereCipher.py
@@ -0,0 +1,34 @@
+def vigenere_encrypt(text, key):
+    encrypted_text = ""
+    key_length = len(key)
+    for i, char in enumerate(text):
+        if char.isalpha():
+            shift = ord(key[i % key_length].upper()) - ord('A')
+            if char.isupper():
+                encrypted_text += chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+            else:
+                encrypted_text += chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+        else:
+            encrypted_text += char
+    return encrypted_text
+
+def vigenere_decrypt(text, key):
+    decrypted_text = ""
+    key_length = len(key)
+    for i, char in enumerate(text):
+        if char.isalpha():
+            shift = ord(key[i % key_length].upper()) - ord('A')
+            if char.isupper():
+                decrypted_text += chr((ord(char) - ord('A') - shift) % 26 + ord('A'))
+            else:
+                decrypted_text += chr((ord(char) - ord('a') - shift) % 26 + ord('a'))
+        else:
+            decrypted_text += char
+    return decrypted_text
+
+text = "HELLO WORLD"
+key = "KEY"
+encrypted = vigenere_encrypt(text, key)
+decrypted = vigenere_decrypt(encrypted, key)
+print("Encrypted:", encrypted)
+print("Decrypted:", decrypted)
